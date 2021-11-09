f9077948a2b8249bf6030a779506b33f744bc2b6
AdhikarShinde <adhikar.shinde@coditas.com>
2021-11-09 11:27:22 +0530

    Fixed the checkbox issue

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,6 +3,8 @@ import AddItem from "../AddItem.tsx/AddItem";
 import styles from "./TodoList.module.scss";
 import  "./TodoList.module.scss";
 import { Data }from "./Todo.types";
+import { computeHeadingLevel } from "@testing-library/dom";
+
 interface Provider {
     completed: boolean;
     todo: string;
@@ -10,6 +12,7 @@ interface Provider {
 
 const TodoList = () => {
     const [todos, setTodos] = useState<Provider[]>([]);
+    const [check, setCheck] = useState(false);
     useEffect(() => {
        setTodos(Data);
     }, []);
@@ -22,7 +25,7 @@ const TodoList = () => {
         const FilteredTodos = todos.filter(ele => ele.todo !== delTodo);
         setTodos(FilteredTodos);
     }
-    const CompletedTodo = (e: any, completedTodo: string) => {
+    const CompletedTodo = (e: any,completedTodo: string) => {
         if (e.target.checked){
             const FilteredTodos = todos.map(ele => {
                 if(ele.todo === completedTodo){
@@ -35,12 +38,32 @@ const TodoList = () => {
         }  
        
     }
+    const UndoCompletedTodo = (e: any,completedTodo: string) => {
+        
+            const FilteredTodos = todos.map(ele => {
+                if(ele.todo === completedTodo){
+                    ele.completed = false;
+                    return ele;
+                }
+                return ele;
+            });
+            setTodos(FilteredTodos);
+          
+       
+    }
     return <>
         {
             todos.map(ele => <div className={styles.flexListItems} key={Math.random()}>
                 {(ele.completed) ? <p className={styles.todo}><del>{ele.todo}</del></p> : <p className={styles.todo}>{ele.todo}</p> }
                 <div className={styles.action}>
-                    <input onChange={(e) => CompletedTodo(e,ele.todo)} type="checkbox" />
+                    <input type="checkbox" checked={ele.completed} onChange={(e) => {
+                        if(ele.completed){
+                            UndoCompletedTodo(e,ele.todo);
+                        }else{
+                            CompletedTodo(e,ele.todo);
+                        }
+                        
+                    }} />
                     <button className={styles.delBtn} onClick={() => DeleteTodo(ele.todo)}><i className="fas fa-trash"></i></button>
                 </div>
             </div>)
